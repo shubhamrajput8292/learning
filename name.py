8372d976812bd86295d768ca2d6e6b6d1e8f2983
shubhamrajput8292 <shubhamsinghrajput8292@gmail.com>
2026-07-28 22:33:22 +0530

    name

---
+++ name.py
@@ -0,0 +1,8 @@
+movies=[]
+movies1=input("enter the movies name")
+movies2=input("enter the movies name")
+movies3=input("enter the movies name")
+movies.append(movies1)
+movies.append(movies2)  
+movies.append(movies3)
+print(movies)
